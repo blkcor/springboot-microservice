91ce4c0eafa1eee8f7d5b62262f523f44bbfc9d2
blkcor <1029129867@qq.com>
2023-04-12 22:24:43 +0800

    chore:remove test code

--- inventory-service/src/main/java/com/chen/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/chen/inventoryservice/service/InventoryService.java
@@ -20,9 +20,6 @@ public class InventoryService {
     @Transactional(readOnly = true)
     @SneakyThrows
     public List<InventoryResponse> isStock(List<String> skuCode) {
-        log.info("Wait Started");
-        Thread.sleep(10000);
-        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory -> InventoryResponse.builder().skuCode(inventory.getSkuCode()).isInStock(inventory.getQuantity() > 0).build()).collect(Collectors.toList());
     }
